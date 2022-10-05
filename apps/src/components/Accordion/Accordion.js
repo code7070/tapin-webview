77230150d4fa4fcdffdaa42ba4dba7abffc6af3c
Jojo <aditya.abdul@weekendinc.com>
2022-10-05 14:58:27 +0700

    ajust complete classname

--- apps/src/components/Accordion/Accordion.js
+++ apps/src/components/Accordion/Accordion.js
@@ -16,17 +16,26 @@ export default function Accordion({
     setOpen(isOpen);
   }, [isOpen]);
 
-  const color = open ? "400" : "200";
+  let colors = "text-ottoBlue-200";
+  let bg = "bg-ottoBlue-200";
+  if (open) {
+    colors = "text-ottoBlue-400";
+    bg = "bg-ottoBlue-400";
+  }
 
   return (
     <div className={`mb-2.5 ${style.accordion} ${open ? style.opened : ""}`}>
       <button
-        className={`btn outline-none border-0 rounded-none block w-full overflow-hidden text-ellipsis flex justify-between normal-case font-normal text-lg py-5 px-7 h-auto bg-ottoBlue-900 hover:bg-ottoBlue-700 text-ottoBlue-${color}`}
+        className={`btn outline-none border-0 rounded-none block w-full overflow-hidden flex justify-between py-5 px-7 h-auto bg-ottoBlue-900 hover:bg-ottoBlue-700`}
         onClick={onClick}
       >
-        <div>{title}</div>
         <div
-          className={`flex [&>*]:ease-in-out [&>*]:duration-500 [&>*]:bg-ottoBlue-${color} [&>*]:w-px [&>*]:h-2`}
+          className={`text-ellipsis normal-case font-normal text-lg ${colors}`}
+        >
+          {title}
+        </div>
+        <div
+          className={`flex [&>*]:ease-in-out [&>*]:duration-500 [&>*]:${bg} [&>*]:w-px [&>*]:h-2`}
         >
           <div className={`translate-x-px ${open ? "rotate-90" : ""}`} />
           <div className={`rotate-90`} />
--- apps/tailwind.config.js
+++ apps/tailwind.config.js
@@ -1,35 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{html,js}", "./src/**/**/*.{html,js}"],
   theme: {
-    colors: {
-      ...colors,
-      ottoGrey: {
-        100: "#4f4f4f",
-        200: "#778892",
-        300: "#828282",
-        400: "#EBEFF0",
-        500: "#E1E1E1",
-        600: "#F2F2F2",
-        700: "#CAD6DA",
-      },
-      ottoBlue: {
-        100: "#002E59",
-        150: "#0E72B0",
-        200: "#138DD7",
-        300: "#2D3944",
-        400: "#0F3B63",
-        500: "#5B97B8",
-        700: "#E5FAFE",
-        900: "#F6F9FA",
+    extend: {
+      colors: {
+        ottoGrey: {
+          100: "#4f4f4f",
+          200: "#778892",
+          300: "#828282",
+          400: "#EBEFF0",
+          500: "#E1E1E1",
+          600: "#F2F2F2",
+          700: "#CAD6DA",
+        },
+        ottoBlue: {
+          100: "#002E59",
+          150: "#0E72B0",
+          200: "#138DD7",
+          300: "#2D3944",
+          400: "#0F3B63",
+          500: "#5B97B8",
+          700: "#E5FAFE",
+          900: "#F6F9FA",
+        },
       },
     },
-    extend: {},
   },
   plugins: [require("daisyui")],
+  safelist: [
+    // {pattern:/(bg|text|border)-ottoBlue(Purple|Pink|Orange|Yellow|Lime|Mint|Test|Test2)/}
+  ],
   daisyui: {
     themes: [
       {

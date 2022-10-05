853c46c3f966d58c6dafe8791fc75f9d82a553b4
Jojo <aditya.abdul@weekendinc.com>
2022-10-05 18:29:37 +0700

    remove testing page

--- apps/src/App.js
+++ apps/src/App.js
@@ -5,7 +5,6 @@ import "./scss/global.scss";
 const Test = lazy(() => import("./pages/Test/Test"));
 const InsuranceList = lazy(() => import("./pages/Insurance/InsuranceList"));
 const InsuranceDetail = lazy(() => import("./pages/Insurance/InsuranceDetail"));
-const Huy = lazy(() => import("./pages/Huy/Huy"));
 
 const App = () => (
   <div className="App">
@@ -16,9 +15,6 @@ const App = () => (
           <Route path="insurance">
             <Route path="list" element={<InsuranceList />} />
             <Route path=":id" element={<InsuranceDetail />} />
-          </Route>
-          <Route path="huy">
-            <Route index element={<Huy />} />
           </Route>
         </Route>
       </Routes>

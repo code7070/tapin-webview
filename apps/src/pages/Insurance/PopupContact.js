49f87c6b22e9664ebe7467421cf328523ff431f7
Jojo <aditya.abdul@weekendinc.com>
2022-10-24 05:12:09 +0700

    add auid apple maps address

--- apps/src/pages/Insurance/PopupContact.js
+++ apps/src/pages/Insurance/PopupContact.js
@@ -38,13 +38,13 @@ const linkAddress = {
     "https://maps.google.com/?q=PT+Equity+Life+Indonesia/@-6.2109142,106.8186699,17z",
   android:
     "https://maps.google.com/?q=PT+Equity+Life+Indonesia/@-6.2109142,106.8186699,17z",
-  ios: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&ll=-6.210914,106.818670&lsp=9902$t=m",
+  ios: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
   apple:
-    "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&ll=-6.210914,106.818670&lsp=9902$t=m",
-  mac: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&ll=-6.210914,106.818670&lsp=9902$t=m",
-  ipad: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&ll=-6.210914,106.818670&lsp=9902$t=m",
+    "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
+  mac: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
+  ipad: "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
   iphone:
-    "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&ll=-6.210914,106.818670&lsp=9902$t=m",
+    "maps://?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
   appleMaps:
     "https://maps.apple.com/?address=Jl.%20Jend.%20Sudirman%20No.%2086,%20Central%20Jakarta,%20Jakarta%2010220,%20Indonesia&auid=6459330042965116742&ll=-6.210914,106.818670&lsp=9902$t=m",
 };

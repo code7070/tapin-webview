3d8ca4265d1616b68642bf5837dd3c4cd2853d51
Jojo <aditya.abdul@weekendinc.com>
2022-10-24 12:14:34 +0700

    remove unnecessary address

--- apps/src/pages/Insurance/PopupContact.js
+++ apps/src/pages/Insurance/PopupContact.js
@@ -79,44 +79,6 @@ export default function PopupContact({ isOpen = true, onClose = () => {} }) {
         linkHref={addressLink}
         linkText="PT Equity Life Indonesia, Sahid Sudirman Center lantai 43 Jl. Jend Sudirman No. 86, Jakarta 10220"
       />
-      <hr />
-      <br />
-      <ContactBox
-        name="Alamat - maps.google.com/?q="
-        icon="Home"
-        linkHref="https://maps.google.com/?q=PT+Equity+Life+Indonesia/@-6.2109142,106.8186699,17z"
-      />
-      <ContactBox
-        name="Alamat - goo.gl/maps"
-        icon="Home"
-        linkHref="https://goo.gl/maps/Dwk9dG9fvVxmWt647"
-      />
-      <ContactBox
-        name="Alamat - comgooglemaps://"
-        icon="Home"
-        linkHref="comgooglemaps://?q=PT+Equity+Life+Indonesia&center=@-6.2109142,106.8186699&zoom=17"
-      />
-      <br />
-      <ContactBox
-        name="Alamat - maps://"
-        icon="Home"
-        linkHref="maps://?q=PT+Equity+Life+Indonesia&sll=-6.2109142,106.8186699&z=17"
-      />
-      <ContactBox
-        name="Alamat - maps://"
-        icon="Home"
-        linkHref="maps://?q=PT+Equity+Life+Indonesia&ll=-6.2109142,106.8186699&z=17"
-      />
-      <ContactBox
-        name="Alamat - maps://"
-        icon="Home"
-        linkHref="maps://?q=PT+Equity+Life+Indonesia&ll=-6.2109142,106.8186699&z=17"
-      />
-      <ContactBox
-        name="Alamat - maps://"
-        icon="Home"
-        linkHref="maps://?address=PT+Equity+Life+Indonesia,Jakarta,Indonesia"
-      />
     </Modal>
   );
 }
